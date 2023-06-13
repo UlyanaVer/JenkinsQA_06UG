d0e5de4e73b1ef7656fbf22ff8a276d5941434ac
Anna Morozova <morozanna503@gmail.com>
2023-06-13 11:51:01 +0400

    refactoring- rf to POM one test (#2559)

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.FolderConfigPage;
 import school.redrover.model.FreestyleProjectConfigPage;
 import school.redrover.model.FreestyleProjectPage;
 import school.redrover.model.MainPage;
@@ -46,16 +47,19 @@ public class DashboardTest extends BaseTest {
     }
 
     @Test
-    public void testReturnToDashboardPage() {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("One");
-        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+    public void testReturnToDashboardPageFromProjectPage() {
+        final String nameProject = "One";
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']"))).click();
-        getDriver().findElement(By.xpath("//a[@class='model-link'][contains(text(),'Dashboard')]")).click();
+        String nameProjectOnMainPage = new MainPage(getDriver())
+                .clickNewItem()
+                .enterItemName(nameProject)
+                .selectJobType(TestUtils.JobType.FreestyleProject)
+                .clickOkButton(new FreestyleProjectConfigPage(new FreestyleProjectPage(getDriver())))
+                .clickSaveButton()
+                .clickDashboard()
+                .getProjectNameMainPage(nameProject);
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='job/One/']")).getText(), "One");
+        Assert.assertEquals(nameProjectOnMainPage,nameProject);
     }
 
     @Test
