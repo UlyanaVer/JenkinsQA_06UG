2fce653156f1a53f0d3b04ad5e009cd21116f649
Anastasia Biletska <114147000+biletskaanastasiiia@users.noreply.github.com>
2023-06-14 18:01:25 +0300

    RF 00.00| New Item> Create New Item With Duplicate Name (#2575)
    * refactor  testCreateNewItemWithSameName
     and deleted sames test

--- src/test/java/school/redrover/NewItemTest.java
+++ src/test/java/school/redrover/NewItemTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -8,18 +9,22 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.model.*;
 import school.redrover.model.NewJobPage;
+import school.redrover.model.base.BaseConfigPage;
+import school.redrover.model.base.BaseMainHeaderPage;
+import school.redrover.model.base.BasePage;
+import school.redrover.model.base.BaseProjectPage;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.TestUtils;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class NewItemTest extends BaseTest {
 
-
     @DataProvider(name = "jobType")
-    public Object[][] JobTypes() {
+    public Object[][] JobTypes(){
         return new Object[][]{
                 {TestUtils.JobType.FreestyleProject},
                 {TestUtils.JobType.Pipeline},
@@ -30,7 +35,7 @@ public class NewItemTest extends BaseTest {
     }
 
     @Test(dataProvider = "jobType")
-    public void testCreateNewItemWithEmptyName(TestUtils.JobType jobType) {
+    public void testCreateNewItemWithEmptyName(TestUtils.JobType jobType){
         String errorMessage = new MainPage(getDriver())
                 .clickNewItem()
                 .selectJobType(jobType)
@@ -40,7 +45,7 @@ public class NewItemTest extends BaseTest {
     }
 
     @Test
-    public void testNewItemHeader() {
+    public void testNewItemHeader(){
         String titleNewItem = new MainPage(getDriver())
                 .clickNewItem()
                 .getTitle();
@@ -49,7 +54,7 @@ public class NewItemTest extends BaseTest {
     }
 
     @Test
-    public void testVerifyNewItemsList() {
+    public void testVerifyNewItemsList(){
         List<String> listOfNewItemsExpect = Arrays.asList("Freestyle project", "Pipeline", "Multi-configuration project", "Folder", "Multibranch Pipeline", "Organization Folder");
 
         List<String> listOfNewItems = new MainPage(getDriver())
@@ -62,7 +67,7 @@ public class NewItemTest extends BaseTest {
     }
 
     @Test
-    public void testVerifyButtonIsDisabled() {
+    public void testVerifyButtonIsDisabled(){
         boolean buttonIsEnabled = new MainPage(getDriver())
                 .clickNewItem()
                 .okButtonIsEnabled();
@@ -70,53 +75,10 @@ public class NewItemTest extends BaseTest {
         Assert.assertFalse(buttonIsEnabled);
     }
 
-    public void createProject(String nameOfProject, String typeOfProject){
-        getDriver().findElement(By.className("task-icon-link")).click();
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
-                By.id("name"))).sendKeys(nameOfProject);
-
-        List <WebElement> listItemOptions = getDriver().findElements(By.id("j-add-item-type-standalone-projects"));
-        for(WebElement element:listItemOptions){
-            if (element.getText().contains(typeOfProject)){
-                element.click();
-            }
-        }
-    }
-
-    @Test
-    public void testCreatePipelineProject(){
-        String expectedResult = "New pipeline project";
-        String typeOfProject = "Pipeline";
-        createProject(expectedResult, typeOfProject);
-        getDriver().findElement(By.id("ok-button")).click();
-
-        getDriver().findElement(By.xpath("//*[text()='Dashboard']")).click();
-
-        List <WebElement> listOfCreateProjects = getDriver().findElements(By.xpath("//table[@id='projectstatus']//a[@class='jenkins-table__link model-link inside']"));
-        List <String> nameOfCreateProjects = new ArrayList<>();
-        for(WebElement element : listOfCreateProjects){
-            nameOfCreateProjects.add(element.getText());
-        }
-
-        Assert.assertEquals(nameOfCreateProjects.get(0), expectedResult);
-        Assert.assertEquals(nameOfCreateProjects.size(), 1);
-    }
-
     @DataProvider(name = "wrong-character")
-    public Object[][] provideWrongCharacters() {
+    public Object[][] provideWrongCharacters(){
         return new Object[][]
                 {{"!"}, {"@"}, {"#"}, {"$"}, {"%"}, {"^"}, {"&"}, {"*"}, {":"}, {";"}, {"/"}, {"|"}, {"?"}, {"<"}, {">"}};
-    }
-
-    @Test(dataProvider = "wrong-character")
-    public void testCreatePipelineProjectWithInvalidName2(String wrongCharacter){
-        createProject(wrongCharacter, "Pipeline");
-
-        String validationMessage = getDriver().findElement(By.id("itemname-invalid")).getText();
-        Assert.assertEquals(validationMessage, "» ‘" + wrongCharacter + "’ is an unsafe character");
-        Assert.assertFalse(getDriver().findElement(By.id("ok-button")).isEnabled());
-
-        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
     }
 
     @Test(dataProvider = "wrong-character")
@@ -127,34 +89,42 @@ public class NewItemTest extends BaseTest {
 
         Assert.assertEquals(newJobPage.getItemInvalidMessage(), "» ‘" + wrongCharacter + "’ is an unsafe character");
         Assert.assertFalse(newJobPage.isOkButtonEnabled());
-        }
-
-    @Test
-    public void testCreatePipelineProjectSameNamed(){
-        String expectedResult = "New Pipeline project";
-        String typeOfProject = "Pipeline";
-        createProject(expectedResult, typeOfProject);
-        getDriver().findElement(By.id("ok-button")).click();
-
-        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
-
-        createProject(expectedResult, typeOfProject);
-
-        String validationMessage = getDriver().findElement(By.id("itemname-invalid")).getText();
-
-        Assert.assertEquals(validationMessage, String.format("» A job already exists with the name ‘%s’", expectedResult));
     }
 
-    @Test
-    public void testCreateMultibranchPipeline(){
-        String project = new MainPage(getDriver())
-                .clickNewItem()
-                .enterItemName("MultibranchPipeline_Project")
-                .selectJobType(TestUtils.JobType.MultibranchPipeline)
-                .clickOkButton(new MultibranchPipelineConfigPage(new MultibranchPipelinePage(getDriver())))
-                .clickSaveButton()
-                .getTextFromNameMultibranchProject();
 
-        Assert.assertEquals(project,"MultibranchPipeline_Project");
+    public void createJob(TestUtils.JobType jobType, String projectName){
+        BaseConfigPage<?, ?> page = null;
+        switch (jobType) {
+            case FreestyleProject -> page = new FreestyleProjectConfigPage(new FreestyleProjectPage(getDriver()));
+            case Pipeline -> page = new PipelineConfigPage(new PipelinePage(getDriver()));
+            case MultiConfigurationProject ->
+                    page = new MultiConfigurationProjectConfigPage(new MultiConfigurationProjectPage(getDriver()));
+            case MultibranchPipeline ->
+                    page = new MultibranchPipelineConfigPage(new MultibranchPipelinePage(getDriver()));
+            case Folder -> page = new FolderConfigPage(new FolderPage(getDriver()));
+            case OrganizationFolder -> page = new OrganizationFolderConfigPage(new OrganizationFolderPage(getDriver()));
+        }
+
+        new MainPage(getDriver())
+                .clickNewItem()
+                .enterItemName(projectName)
+                .selectJobType(jobType)
+                .clickOkButton(page)
+                .clickSaveButton()
+                .getHeader()
+                .clickLogo();
+    }
+
+    @Test(dataProvider = "jobType")
+    public void testCreateNewItemWithDuplicateNamed(TestUtils.JobType jobType){
+        String projectName = "project";
+        createJob(jobType, projectName);
+        String validationMessage = new MainPage(getDriver())
+                .clickNewItem()
+                .enterItemName(projectName)
+                .selectJobType(jobType)
+                .getItemInvalidMessage();
+
+        Assert.assertEquals(validationMessage, String.format("» A job already exists with the name ‘%s’", projectName));
     }
 }
