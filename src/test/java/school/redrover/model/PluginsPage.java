d2de27312f31f4b12c533174a7f7f8aaf1fca6dd
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-06-17 19:53:52 +0300

    RF PlaginsPage add FindBy (#2612)

--- src/test/java/school/redrover/model/PluginsPage.java
+++ src/test/java/school/redrover/model/PluginsPage.java
@@ -2,15 +2,19 @@ package school.redrover.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class PluginsPage extends BaseMainHeaderPage<PluginsPage> {
+    @FindBy(xpath = "//h1")
+    private WebElement pageTitle;
 
     public PluginsPage(WebDriver driver) {
         super(driver);
     }
 
     public String getPageTitle() {
-        return getDriver().findElement(By.xpath("//h1")).getText();
+        return pageTitle.getText();
     }
 }
