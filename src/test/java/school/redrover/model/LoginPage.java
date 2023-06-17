f2ce19aa275d61bbe4b39b7b824b4c757f2b165e
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-06-17 16:42:51 +0300

    RF LoginPage withFindBy (#2608)

--- src/test/java/school/redrover/model/LoginPage.java
+++ src/test/java/school/redrover/model/LoginPage.java
@@ -2,30 +2,43 @@ package school.redrover.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BaseModel;
 
 public class  LoginPage extends BaseModel {
+    @FindBy(xpath = "//input[@name='j_username']")
+    private WebElement inputUserNameField;
+
+    @FindBy (xpath = "//input[@name='j_password']")
+    private WebElement inputPasswordField;
+
+    @FindBy(xpath = "//div[text()='Invalid username or password']")
+    private WebElement incorrectUserNameOrPassword;
+
+    @FindBy (xpath = "//button[@name='Submit']")
+    private WebElement signInButton;
 
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
     public LoginPage enterUsername(String userName) {
-        getDriver().findElement(By.xpath("//input[@name='j_username']")).sendKeys(userName);
+        inputUserNameField.sendKeys(userName);
         return this;
     }
 
     public LoginPage enterPassword(String password) {
-        getDriver().findElement(By.xpath("//input[@name='j_password']")).sendKeys(password);
+        inputPasswordField.sendKeys(password);
         return this;
     }
 
     public <Page extends BaseModel> Page enterSignIn(Page page) {
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        signInButton.click();
         return page;
     }
 
     public String getTextAlertIncorrectUsernameOrPassword() {
-       return getDriver().findElement(By.xpath("//div[text()='Invalid username or password']")).getText();
+       return incorrectUserNameOrPassword.getText();
     }
 }
