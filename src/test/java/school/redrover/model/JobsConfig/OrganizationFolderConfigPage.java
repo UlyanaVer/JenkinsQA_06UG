a66bf64b8f9d2454596de23e0db28395c2b69ae6
Anna Morozova <morozanna503@gmail.com>
2023-06-18 07:49:35 +0400

    RF | OrganizationFolderConfigPage > added FindBy on OrgFolderConfig (#2615)
    * added FindBy on OrgFolderConfig
    
    * added Ignor testFreestyleProjectJob()

--- src/test/java/school/redrover/model/JobsConfig/OrganizationFolderConfigPage.java
+++ src/test/java/school/redrover/model/JobsConfig/OrganizationFolderConfigPage.java
@@ -1,17 +1,21 @@
 package school.redrover.model.JobsConfig;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.Jobs.OrganizationFolderPage;
 import school.redrover.model.base.BaseConfigFoldersPage;
 
 public class OrganizationFolderConfigPage extends BaseConfigFoldersPage<OrganizationFolderConfigPage, OrganizationFolderPage> {
 
+    @FindBy(xpath = "//label[@data-title='Disabled']")
+    private WebElement disableFromConfig;
     public OrganizationFolderConfigPage(OrganizationFolderPage organizationFolderPage) {
         super(organizationFolderPage);
     }
 
     public OrganizationFolderConfigPage clickDisable(){
-        getDriver().findElement(By.xpath("//label[@data-title='Disabled']")).click();
+        disableFromConfig.click();
 
         return this;
     }
