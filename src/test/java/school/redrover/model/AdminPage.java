611c7749fd7611fc4cd0fbe139bd90f1bd97b4fb
Lana K <nika.benz.21@gmail.com>
2023-06-18 12:04:44 +0300

    RF | AdminPage > add FindBy (#2625)

--- src/test/java/school/redrover/model/AdminPage.java
+++ src/test/java/school/redrover/model/AdminPage.java
@@ -1,19 +1,21 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class AdminPage extends BaseMainHeaderPage<AdminPage> {
+    @FindBy(css = "#main-panel > div:nth-child(4)")
+    private WebElement title;
 
     public AdminPage(WebDriver driver) {
         super(driver);
     }
 
     public String getTitleText() {
-        return getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#main-panel > div:nth-child(4)"))).getText();
-
+        return getWait2().until(ExpectedConditions.visibilityOf(title)).getText();
     }
 }
 
