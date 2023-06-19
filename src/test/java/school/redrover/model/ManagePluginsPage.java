364bfcbb576f4909b3fb88b784789bf5cf3f72dc
Evgeniia Demchenko <demchenkoeb04022012@gmail.com>
2023-06-19 08:29:47 +0300

    RF ManagePluginsPage_add_FindBy (#2645)

--- src/test/java/school/redrover/model/ManagePluginsPage.java
+++ src/test/java/school/redrover/model/ManagePluginsPage.java
@@ -1,8 +1,8 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.runner.TestUtils;
@@ -11,19 +11,24 @@ import java.util.List;
 
 public class ManagePluginsPage extends BaseMainHeaderPage<ManagePluginsPage> {
 
+    @FindBy(xpath = "//div[@id='tasks']//div[not(contains(@class, 'subtask'))]")
+    private List<WebElement> fourTasksOnTheLeftsidePanel;
+
+    @FindBy(xpath = "//*[@id='tasks']/div[4]/span")
+    private WebElement advancedSettings;
+
     public ManagePluginsPage(WebDriver driver) {
         super(driver);
     }
+
     public List<String> checkFourTasksOnTheLeftsidePanel() {
-        List<WebElement> listOfTasks = getWait5().until(ExpectedConditions.presenceOfAllElementsLocatedBy
-                (By.xpath("//div[@id='tasks']//div[not(contains(@class, 'subtask'))]")));
+        List<WebElement> listOfTasks = getWait5().until(ExpectedConditions.visibilityOfAllElements(fourTasksOnTheLeftsidePanel));
 
         return TestUtils.getTexts(listOfTasks);
     }
 
     public AdvancedSettingsPage clickAdvancedSettings(){
-        getDriver().findElement(By.xpath("//*[@id='tasks']/div[4]/span"))
-                .click();
+        advancedSettings.click();
         return new AdvancedSettingsPage(getDriver());
     }
 }
