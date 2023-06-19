0390d82920a68878697aa00c09f2918d1d9e8589
Artem Savichev <90255887+SavaSavichev@users.noreply.github.com>
2023-06-19 17:51:41 +0600

    RF 01.01 | Freestyle project > Add FindBy (#2649)

--- src/test/java/school/redrover/model/jobs/FreestyleProjectPage.java
+++ src/test/java/school/redrover/model/jobs/FreestyleProjectPage.java
@@ -2,6 +2,7 @@ package school.redrover.model.jobs;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.*;
 import school.redrover.model.jobsconfig.FreestyleProjectConfigPage;
@@ -12,6 +13,66 @@ import java.util.List;
 import static org.openqa.selenium.By.cssSelector;
 
 public class FreestyleProjectPage extends BaseProjectPage<FreestyleProjectPage> {
+
+    @FindBy(id = "description-link")
+    private WebElement addDescriptionButton;
+
+    @FindBy(xpath = "//*[@href = 'editDescription']")
+    private WebElement editDescriptionButton;
+
+    @FindBy(xpath = "//*[@id='description']/form/div[2]/button")
+    private WebElement saveDescriptionButton;
+
+    @FindBy(xpath = "//textarea[@name='description']")
+    private WebElement descriptionTextArea;
+
+    @FindBy(id = "enable-project")
+    private WebElement warningMessage;
+
+    @FindBy(xpath = "//a[@class = 'textarea-show-preview']")
+    private WebElement previewButton;
+
+    @FindBy(xpath = "//*[@class = 'textarea-preview']")
+    private WebElement descriptionPreviewButton;
+
+    @FindBy(name = "Submit")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//ul[@class='permalinks-list']//li")
+    private List<WebElement> permalinks;
+
+    @FindBy(css = "[href$='console']")
+    private WebElement buildItem;
+
+    @FindBy(xpath = "//button[text() = 'Disable Project']")
+    private WebElement disableProjectButton;
+
+    @FindBy(xpath = "//button[text() = 'Enable']")
+    private WebElement enableProjectButton;
+
+    @FindBy(xpath = "//*[@id='description']/div")
+    private WebElement description;
+
+    @FindBy(xpath = "//a[@href = '#']//span[text() = 'Delete Project']")
+    private WebElement deleteButton;
+
+    @FindBy(xpath = "//a[@class='build-status-link']")
+    private WebElement consoleOutputButton;
+
+    @FindBy(linkText = "Dashboard")
+    private WebElement dashboardButton;
+
+    @FindBy(xpath = "//a[contains(@href, 'changes')]")
+    private WebElement changesButton;
+
+    @FindBy(xpath = "//a[@class='model-link' and contains(@href,'job')]")
+    private WebElement jobOnBreadcrumbBarDropDownMenuButton;
+
+    @FindBy(xpath = "//a[@class='model-link' and contains(@href,'job')]/button")
+    private WebElement optionsBreadcrumbBarDropDownMenuButton;
+
+    @FindBy(xpath = "//a[@class='yuimenuitemlabel' and @href='#']/span[text()='Delete Project']/..")
+    private WebElement deleteButtonOnDropDownMenu;
 
     public FreestyleProjectPage(WebDriver driver) {
         super(driver);
@@ -24,121 +85,108 @@ public class FreestyleProjectPage extends BaseProjectPage<FreestyleProjectPage> 
     }
 
     public BuildPage selectBuildItemTheHistoryOnBuildPage() {
-        getWait10().until(ExpectedConditions
-                .visibilityOfElementLocated(cssSelector("[href$='console']"))).click();
+        getWait10().until(ExpectedConditions.visibilityOf(buildItem)).click();
         return new BuildPage(getDriver());
     }
 
     public FreestyleProjectPage clickTheDisableProjectButton() {
         getWait5().until(ExpectedConditions
-                .elementToBeClickable(By.xpath("//button[text() = 'Disable Project']"))).click();
+                .elementToBeClickable(disableProjectButton)).click();
         return this;
     }
 
     public FreestyleProjectPage clickTheEnableProjectButton() {
-        getWait5().until(ExpectedConditions
-                .elementToBeClickable(By.xpath("//button[text() = 'Enable']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(enableProjectButton)).click();
         return this;
     }
 
     public String getDescription() {
-        return getDriver().findElement(By.xpath("//*[@id='description']/div")).getText();
+        return description.getText();
     }
 
     public FreestyleProjectPage clickAddDescription() {
-        getDriver().findElement(By.id("description-link")).click();
+        addDescriptionButton.click();
         return this;
     }
 
     public FreestyleProjectPage clickEditDescription() {
-        getDriver().findElement(By.xpath("//*[@href = 'editDescription']")).click();
+        editDescriptionButton.click();
         return this;
     }
 
     public FreestyleProjectPage clickSaveDescription() {
-        getDriver().findElement(By.xpath("//*[@id='description']/form/div[2]/button")).click();
+        saveDescriptionButton.click();
         return this;
     }
 
     public FreestyleProjectPage addDescription(String description) {
-        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys(description);
+        descriptionTextArea.sendKeys(description);
         return this;
     }
 
     public FreestyleProjectPage removeOldDescriptionAndAddNew (String description) {
-        WebElement oldDescription = getDriver().findElement(By.xpath("//*[@id='description']/form/div[1]/div[1]/textarea"));
-        oldDescription.clear();
-        oldDescription.sendKeys(description);
+        descriptionTextArea.clear();
+        descriptionTextArea.sendKeys(description);
         return this;
     }
 
     public String  getWarningMessage() {
-
-        return getDriver().findElement(By.id("enable-project")).getText().substring(0,34);
+        return warningMessage.getText().substring(0,34);
     }
 
     public FreestyleProjectPage clickPreviewButton () {
-        getDriver().findElement(By.xpath("//a[@class = 'textarea-show-preview']")).click();
+        previewButton.click();
         return this;
     }
 
     public String getPreviewDescription () {
-        return getDriver().findElement(By.xpath("//*[@class = 'textarea-preview']")).getText();
+        return descriptionPreviewButton.getText();
     }
 
     public MainPage clickDeleteProject() {
-        getWait2().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[@href = '#']//span[text() = 'Delete Project' ]"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(deleteButton)).click();
         Alert alert = getDriver().switchTo().alert();
         alert.accept();
         return new MainPage(getDriver());
     }
 
     public ConsoleOutputPage openConsoleOutputForBuild(){
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[@class='build-status-link']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(consoleOutputButton)).click();
         return new ConsoleOutputPage(getDriver());
     }
 
     public FreestyleProjectPage clickSaveButton() {
-        getDriver().findElement(By.name("Submit")).click();
+        saveButton.click();
         return new FreestyleProjectPage(getDriver());
     }
 
     public int getSizeOfPermalinksList() {
         getWait2().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2")));
-
-        List<WebElement> permalinks = getDriver()
-                .findElements(By.xpath("//ul[@class='permalinks-list']//li"));
-
         return permalinks.size();
     }
 
     public MainPage clickDashboard() {
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
-                By.linkText("Dashboard"))).click();
+        getWait2().until(ExpectedConditions.visibilityOf(dashboardButton)).click();
         return new MainPage(getDriver());
     }
 
     public ChangesPage<FreestyleProjectPage> clickChangeOnLeftSideMenu() {
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@href, 'changes')]"))).click();
+        getWait10().until(ExpectedConditions.visibilityOf(changesButton)).click();
         return new ChangesPage<>(this);
     }
 
     private void  openJobOnBreadcrumbBarDropDownMenu() {
-        new Actions(getDriver()).moveToElement(getDriver().findElement(By
-                .xpath("//a[@class='model-link' and contains(@href,'job')]"))).perform();
+        new Actions(getDriver()).moveToElement(jobOnBreadcrumbBarDropDownMenuButton).perform();
 
-        WebElement options = getWait2().until(ExpectedConditions.elementToBeClickable(By
-                .xpath("//a[@class='model-link' and contains(@href,'job')]/button")));
+        WebElement options = getWait2().until(
+                ExpectedConditions.elementToBeClickable(optionsBreadcrumbBarDropDownMenuButton));
         new Actions(getDriver()).moveToElement(options).perform();
         options.sendKeys(Keys.RETURN);
     }
 
     public FreestyleProjectPage clickDeleteProjectOnDropDown() {
         openJobOnBreadcrumbBarDropDownMenu();
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By
-                        .xpath("//a[@class='yuimenuitemlabel' and @href='#']/span[text()='Delete Project']/.."))).click();
+        getWait2().until(ExpectedConditions.visibilityOf(deleteButtonOnDropDownMenu)).click();
         return this;
     }
 
