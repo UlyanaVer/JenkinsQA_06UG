6fb9197d2ab3ee40bcfcf4948ade50501e9d42ac
Vira Dmytrenko <dvn2909@gmail.com>
2023-06-21 19:20:33 +0300

    Add FindBy (#2691)

--- src/test/java/school/redrover/model/CreateNodePage.java
+++ src/test/java/school/redrover/model/CreateNodePage.java
@@ -1,34 +1,44 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class CreateNodePage extends BaseMainHeaderPage<CreateNodePage> {
+
+    @FindBy(xpath = "//input[@name='name']")
+    private WebElement inputName;
+
+    @FindBy(xpath = "//textarea[@name='nodeDescription']")
+    private WebElement descriptionTextarea;
+
+    @FindBy(xpath = "//button[@name='Submit']")
+    private WebElement saveButton;
 
     public CreateNodePage(WebDriver driver) {
         super(driver);
     }
 
     public CreateNodePage clearNameField() {
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='name']"))).clear();
+        getWait2().until(ExpectedConditions.elementToBeClickable(inputName)).clear();
         return this;
     }
 
     public ErrorNodePage clickSaveButtonWhenNameFieldEmpty() {
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(saveButton)).click();
         return new ErrorNodePage(getDriver());
     }
 
     public ManageNodesPage clickSaveButton() {
-        getWait10().until(ExpectedConditions
-                .elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(saveButton)).click();
         return new ManageNodesPage(getDriver());
     }
+
     public CreateNodePage addDescription(String description) {
         getWait2().until(ExpectedConditions
-                .elementToBeClickable(By.xpath("//textarea[@name='nodeDescription']"))).sendKeys(description);
+                .elementToBeClickable(descriptionTextarea)).sendKeys(description);
         return this;
     }
 }
