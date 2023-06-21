e83b79f62dba310e38100c0ee9b1bf0a0c9065ea
Lana K <nika.benz.21@gmail.com>
2023-06-21 18:18:38 +0300

    RF | MultibranchPipelineConfigPage > Add FindBy (#2689)

--- src/test/java/school/redrover/model/jobsconfig/MultibranchPipelineConfigPage.java
+++ src/test/java/school/redrover/model/jobsconfig/MultibranchPipelineConfigPage.java
@@ -1,6 +1,7 @@
 package school.redrover.model.jobsconfig;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import school.redrover.model.jobs.MultibranchPipelinePage;
@@ -8,22 +9,31 @@ import school.redrover.model.base.BaseConfigFoldersPage;
 
 public class MultibranchPipelineConfigPage extends BaseConfigFoldersPage<MultibranchPipelineConfigPage, MultibranchPipelinePage> {
 
+    @FindBy(xpath = "//label[@data-title='Disabled']")
+    private WebElement disabledSwitch;
+
+    @FindBy(xpath = "//button[@data-section-id='appearance']")
+    private WebElement appearanceButton;
+
+    @FindBy(xpath = "//div[@class='jenkins-form-item has-help']/div/select")
+    private WebElement defaultIcon;
+
     public MultibranchPipelineConfigPage(MultibranchPipelinePage multibranchPipelinePage) {
         super(multibranchPipelinePage);
     }
 
     public MultibranchPipelineConfigPage clickDisable() {
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@data-title='Disabled']"))).click();
+        getWait5().until(ExpectedConditions.visibilityOf(disabledSwitch)).click();
         return this;
     }
 
     public MultibranchPipelineConfigPage clickAppearance() {
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-section-id='appearance']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(appearanceButton)).click();
         return this;
     }
 
     public MultibranchPipelineConfigPage selectDefaultIcon() {
-        new Select(getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='jenkins-form-item has-help']/div/select"))))
+        new Select(getWait5().until(ExpectedConditions.elementToBeClickable(defaultIcon)))
                 .selectByVisibleText("Default Icon");
         return this;
     }
