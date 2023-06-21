769f25c1ba1f6b5b438ffa92868b937bda61303a
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-06-22 01:07:49 +0400

    added FindBy on PeoplePage (#2695)

--- src/test/java/school/redrover/model/PeoplePage.java
+++ src/test/java/school/redrover/model/PeoplePage.java
@@ -3,21 +3,38 @@ package school.redrover.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class PeoplePage extends BaseMainHeaderPage<PeoplePage> {
+
+    @FindBy(css = ".task-link-wrapper>a[href$='newJob']")
+    private WebElement newItem;
+
+    @FindBy(xpath = "//a[@class='sortheader'][contains(text(), 'User ID')]")
+    private WebElement userIDButton;
+
+    @FindBy(xpath = "//a[@class='sortheader'][contains(text(), 'User ID')]/span")
+    private WebElement userIDButtonArrow;
+
+    @FindBy(xpath = "//a[@class='sortheader'][contains(text(), 'Name')]")
+    private WebElement nameButton;
+
+    @FindBy(xpath = "//h1")
+    private WebElement pageTitle;
+
+    @FindBy(xpath = "//table[@id = 'people']/tbody")
+    private WebElement user;
 
     public PeoplePage(WebDriver driver) {
         super(driver);
     }
 
-    public UserPage clickUserName (String newUserName) {
+    public UserPage clickUserName(String newUserName) {
         getWait5().until(ExpectedConditions.elementToBeClickable(getDriver()
                 .findElement(By.xpath("//a[@href='/user/" + newUserName + "/']")))).click();
         return new UserPage(getDriver());
-
     }
 
     public boolean checkIfUserWasDeleted(String newUserName) {
@@ -27,48 +44,37 @@ public class PeoplePage extends BaseMainHeaderPage<PeoplePage> {
     }
 
     public NewJobPage clickNewItem() {
-        getDriver().findElement(By.cssSelector(".task-link-wrapper>a[href$='newJob']")).click();
+        newItem.click();
         return new NewJobPage(getDriver());
     }
 
     public PeoplePage clickUserIDButton() {
-        WebElement userIDButton = getDriver().findElement(
-                By.xpath("//a[@class='sortheader'][contains(text(), 'User ID')]"));
-            userIDButton.click();
-            return new PeoplePage(getDriver());
+        userIDButton.click();
+        return new PeoplePage(getDriver());
     }
 
     public boolean isUserIDButtonWithoutArrow() {
-        WebElement userIDButtonArrow = getDriver().findElement(
-                By.xpath("//a[@class='sortheader'][contains(text(), 'User ID')]/span"));
         return userIDButtonArrow.getText().isEmpty();
     }
 
     public boolean isUserIDButtonWithUpArrow() {
-        WebElement userIDButtonArrow = getDriver().findElement(
-                By.xpath("//a[@class='sortheader'][contains(text(), 'User ID')]/span"));
         return userIDButtonArrow.getText().trim().contains("↑");
     }
 
     public boolean isUserIDButtonWithDownArrow() {
-        WebElement userIDButtonArrow = getDriver().findElement(
-                By.xpath("//a[@class='sortheader'][contains(text(), 'User ID')]/span"));
         return userIDButtonArrow.getText().trim().contains("↓");
     }
 
     public PeoplePage clickNameButton() {
-        WebElement nameButton = getDriver().findElement(
-                By.xpath("//a[@class='sortheader'][contains(text(), 'Name')]"));
         nameButton.click();
         return new PeoplePage(getDriver());
     }
 
     public String getPageTitle() {
-        return getDriver().findElement(By.xpath("//h1")).getText();
+        return pageTitle.getText();
     }
 
     public boolean checkIfUserWasAdded(String userName) {
-        return getDriver().findElement(By.xpath("//table[@id = 'people']/tbody")).getText().contains(userName);
-
+        return user.getText().contains(userName);
     }
 }
