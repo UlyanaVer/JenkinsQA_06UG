774ee9ff75e19dd0eed8466dce2f72334ee896bc
EvanMai <107349235+evanmaiqa@users.noreply.github.com>
2023-06-21 03:20:09 -0700

    RF | RestApiPage add FindBy (#2677)

--- src/test/java/school/redrover/model/RestApiPage.java
+++ src/test/java/school/redrover/model/RestApiPage.java
@@ -1,17 +1,21 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class RestApiPage extends BaseMainHeaderPage<RestApiPage> {
+
+    @FindBy(xpath = "//h1")
+    private WebElement title;
 
     public RestApiPage(WebDriver driver) {
         super(driver);
     }
 
     public String getRestApiPageTitle(){
-       return getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1"))).getText();
+       return getWait2().until(ExpectedConditions.elementToBeClickable(title)).getText();
     }
 }
