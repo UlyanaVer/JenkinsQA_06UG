20153b5edf7b075a98538e68baf9abd8890d641b
Mariia Rykova <maryrykova24@gmail.com>
2023-06-21 16:36:52 +0400

    RF | NodePage > add FindBy (#2685)

--- src/test/java/school/redrover/model/NodePage.java
+++ src/test/java/school/redrover/model/NodePage.java
@@ -2,17 +2,21 @@ package school.redrover.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class NodePage extends BaseMainHeaderPage<NodePage> {
+
+    @FindBy(xpath = "//div[@id='description']/div[1]")
+    private WebElement nodeDescription;
+
     public NodePage(WebDriver driver) {
         super(driver);
     }
 
     public String getNodeDescription() {
-        return getWait5().until(ExpectedConditions
-                        .visibilityOfElementLocated(By.xpath("//div[@id='description']/div[1]")))
-                .getText();
+        return getWait5().until(ExpectedConditions.visibilityOf(nodeDescription)).getText();
     }
 }
