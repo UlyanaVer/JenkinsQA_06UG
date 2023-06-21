b907d865b6c6889788518750d480216cbdbc9336
Lana K <nika.benz.21@gmail.com>
2023-06-21 08:43:58 +0300

    RF | ViewFolderPage > Add FindBy (#2673)

--- src/test/java/school/redrover/model/ViewFolderPage.java
+++ src/test/java/school/redrover/model/ViewFolderPage.java
@@ -1,23 +1,31 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.jobs.FolderPage;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class ViewFolderPage extends BaseMainHeaderPage<ViewFolderPage> {
 
+    @FindBy(linkText = "All")
+    private WebElement allLink;
+
+    @FindBy(xpath = "//div[@class='tab active']")
+    private WebElement myViewTabBar;
+
     public ViewFolderPage(WebDriver driver) {
         super(driver);
     }
 
-    public FolderPage clickAll(){
-        getDriver().findElement(By.linkText("All")).click();
+    public FolderPage clickAll() {
+        allLink.click();
+
         return new FolderPage(getDriver());
     }
 
-    public String  getMyView() {
-        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='tab active']"))).getText();
+    public String getMyView() {
+        return getWait5().until(ExpectedConditions.visibilityOf(myViewTabBar)).getText();
     }
 }
