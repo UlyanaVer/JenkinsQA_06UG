9d9f930415068ccda86d8639d03f2608dc636bf1
Artem Savichev <90255887+SavaSavichev@users.noreply.github.com>
2023-06-21 16:25:37 +0600

    RF ViewConfigPage > add findBy (#2679)

--- src/test/java/school/redrover/model/ViewConfigPage.java
+++ src/test/java/school/redrover/model/ViewConfigPage.java
@@ -1,8 +1,8 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseConfigPage;
 
@@ -11,13 +11,32 @@ import java.util.Objects;
 
 public class ViewConfigPage extends BaseConfigPage<ViewConfigPage, ViewPage> {
 
+    @FindBy(xpath = "//div[@class = 'listview-jobs']/span")
+    private List<WebElement> viewJobList;
+
+    @FindBy(xpath = "//label[contains(text(), 'Recurse in subfolders')]")
+    private WebElement recurseCheckbox;
+
+    @FindBy(id = "yui-gen1-button")
+    private WebElement addJobFilterDropDown;
+
+    @FindBy(xpath = "//*[@previewendpoint='/markupFormatter/previewDescription']")
+    private WebElement preview;
+
+    @FindBy(xpath = "//*[@class='textarea-preview']")
+    private WebElement previewTextArea;
+
+    @FindBy(xpath = "//div/a[@helpurl='/help/view-config/description.html']")
+    private WebElement helpDescriptionButton;
+
+    @FindBy(xpath = "//div[@class='help-area tr']/div/div")
+    private WebElement helpDescriptionTextArea;
+
     public ViewConfigPage(ViewPage viewPage) {
         super(viewPage);
     }
 
     public ViewConfigPage selectJobsInJobFilters (String name) {
-        List<WebElement> viewJobList = getDriver().findElements(By.xpath("//div[@class = 'listview-jobs']/span"));
-
         for (WebElement el : viewJobList) {
             if (Objects.equals(el.getText(), name)) {
                 el.click();
@@ -27,21 +46,18 @@ public class ViewConfigPage extends BaseConfigPage<ViewConfigPage, ViewPage> {
     }
 
     public ViewConfigPage selectRecurseCheckbox() {
-        getWait2().until(ExpectedConditions.elementToBeClickable(
-                getDriver().findElement(By.xpath("//label[contains(text(), 'Recurse in subfolders')]")))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(recurseCheckbox)).click();
 
         return this;
     }
 
     public ViewConfigPage scrollToAddJobFilterDropDown() {
-        new Actions(getDriver()).scrollToElement(getDriver().findElement(By.id("yui-gen1-button"))).perform();
+        new Actions(getDriver()).scrollToElement(addJobFilterDropDown).perform();
 
         return this;
     }
 
     public ViewConfigPage chooseJobsInJobFilters (String name) {
-        List<WebElement> viewJobList = getDriver().findElements(By.xpath("//div[@class = 'listview-jobs']/span"));
-
         for (WebElement el : viewJobList) {
             if (Objects.equals(el.getText(), name)) {
                 el.click();
@@ -51,24 +67,21 @@ public class ViewConfigPage extends BaseConfigPage<ViewConfigPage, ViewPage> {
     }
 
     public ViewConfigPage clickPreview() {
-        getDriver().findElement(
-                By.xpath("//*[@previewendpoint='/markupFormatter/previewDescription']")).click();
+        preview.click();
         return this;
     }
 
     public String getPreviewText() {
-        return getWait2().until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//*[@class='textarea-preview']"))).getText();
+        return getWait2().until(ExpectedConditions.visibilityOf(previewTextArea)).getText();
     }
 
     public ViewConfigPage clickHelpFeatureDescription() {
-        getDriver().findElement(
-                By.xpath("//div/a[@helpurl='/help/view-config/description.html']")).click();
+        helpDescriptionButton.click();
         return this;
     }
 
     public String getTextHelpFeatureDescription() {
         return getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//div[@class='help-area tr']/div/div"))).getText();
+                helpDescriptionTextArea)).getText();
     }
 }
