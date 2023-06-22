1c9ad86388e774d738730900c0fcde10ca53badb
Lana K <nika.benz.21@gmail.com>
2023-06-22 19:11:49 +0300

    RF | TimelinePage, BuiltInNodePage > Add FindBy (#2702)

--- src/test/java/school/redrover/model/BuiltInNodePage.java
+++ src/test/java/school/redrover/model/BuiltInNodePage.java
@@ -1,17 +1,20 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class BuiltInNodePage extends BaseMainHeaderPage<BuiltInNodePage> {
+
+    @FindBy(xpath = "//div[@class='jenkins-app-bar__content']/h1")
+    private WebElement title;
+
     public BuiltInNodePage(WebDriver driver) {
         super(driver);
     }
 
-    public String getTitle(){
-
-        return getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar__content']/h1")).getText();
+    public String getTitle() {
+        return title.getText();
     }
-
 }
--- src/test/java/school/redrover/model/TimelinePage.java
+++ src/test/java/school/redrover/model/TimelinePage.java
@@ -2,6 +2,8 @@ package school.redrover.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
@@ -10,18 +12,22 @@ import java.util.List;
 
 public class TimelinePage extends BaseMainHeaderPage<TimelinePage> {
 
+    @FindBy(css = "[href$='console']")
+    private WebElement buildIcon;
+
     public TimelinePage(WebDriver driver) {
         super(driver);
     }
 
     public ConsoleOutputPage clickBuildIcon() {
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href$='console']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(buildIcon)).click();
+
         return new ConsoleOutputPage(getDriver());
     }
 
-    public List getBuildNumbers(int numbersOfBuild) {
+    public List<String> getBuildNumbers(int numbersOfBuild) {
         List<String> buildNumber = new ArrayList<>();
-        for(int i = 1; i <= numbersOfBuild; i++) {
+        for (int i = 1; i <= numbersOfBuild; i++) {
             buildNumber.add(getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[href='" + i + "/']"))).getText());
         }
         return buildNumber;
