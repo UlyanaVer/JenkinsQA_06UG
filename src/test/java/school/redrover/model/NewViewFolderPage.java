04ef48e85b3ce26e6ab2977959c0a69646efd0bc
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-06-22 06:54:54 +0400

    added FindBy on NewViewFolderPage (#2697)

--- src/test/java/school/redrover/model/NewViewFolderPage.java
+++ src/test/java/school/redrover/model/NewViewFolderPage.java
@@ -1,28 +1,36 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class NewViewFolderPage extends BaseMainHeaderPage<NewViewFolderPage> {
+
+    @FindBy(xpath = "//button[@id='ok']")
+    private WebElement okButton;
+
+    @FindBy(id = "name")
+    private WebElement viewNameInFolder;
+
+    @FindBy(xpath = "//fieldset/div[3]/input")
+    private WebElement myView;
 
     public NewViewFolderPage(WebDriver driver) {
         super(driver);
     }
 
     private WebElement getOkButton() {
-        return getDriver().findElement(By.xpath("//button[@id='ok']"));
+        return okButton;
     }
 
-    public NewViewFolderPage enterViewName(String viewName){
-        getDriver().findElement(By.id("name")).sendKeys(viewName);
+    public NewViewFolderPage enterViewName(String viewName) {
+        viewNameInFolder.sendKeys(viewName);
         return this;
     }
 
-    public ViewFolderPage selectMyViewAndClickCreate(){
-        WebElement myView = getDriver().findElement(By.xpath("//fieldset/div[3]/input"));
+    public ViewFolderPage selectMyViewAndClickCreate() {
         new Actions(getDriver()).moveToElement(myView).click(myView).perform();
         getOkButton().click();
         return new ViewFolderPage(getDriver());
